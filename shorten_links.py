8a62f01549c41502bd13cb3df93c995ce8875248
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-03-19 20:05:48 +0300

    Add Bitly service HTTPError handling to shorten_link
    This commit adds one more try-except block to shorten_link() function
    to handle the case when an HTTPError answer received from the Bitly
    service, preventing code from crashing and printing a user-friendly
    error message instead.

--- shorten_links.py
+++ shorten_links.py
@@ -11,24 +11,26 @@ def shorten_link(url=input("Введите ссылку, которую хоти
     except requests.exceptions.ConnectionError:
         return "Ошибка! Введена неверная ссылка."
 
+    try:
+        request_url = 'https://api-ssl.bitly.com/v4/shorten'
 
-    request_url = 'https://api-ssl.bitly.com/v4/shorten'
+        headers = {
+            'Authorization': token,
+            'Content-Type': 'application/json',
+        }
 
-    headers = {
-        'Authorization': token,
-        'Content-Type': 'application/json',
-    }
+        data = {
+            "long_url": url,
+            "domain": "bit.ly",
+            "group_guid": group_guid,
+        }
 
-    data = {
-        "long_url": url,
-        "domain": "bit.ly",
-        "group_guid": group_guid,
-    }
-
-    data_json = json.dumps(data)
-    response = requests.post(request_url, headers=headers, data=data_json)
-    response.raise_for_status()
-    return response.json()['id']
+        data_json = json.dumps(data)
+        response = requests.post(request_url, headers=headers, data=data_json)
+        response.raise_for_status()
+        return response.json()['id']
+    except requests.exceptions.HTTPError:
+        return "Ошибка! Сервис Bitly недоступен."
 
 
 print(shorten_link())
